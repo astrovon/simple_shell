e0716c4f43d6cd10c7f5223d6ca7af42b9ae340f
astrovon <perpetual2005@gmail.com>
2023-11-15 15:33:18 -0800

    subshell.h

--- main.c
+++ main.c
@@ -1,28 +1,74 @@
 #include "subshell.h"
 
-
 /**
- * main - entry point
+ * free_data - frees data structure
  *
- * @argc: number of arguments given to the program
- * @argv: arguments list
- *
- * Return: returns the value of the last executed command
+ * @datash: data structure
+ * Return: no return
  */
-int main(int argc, char **argv)
+
+void free_data(data_shell *datash)
 {
-	char *cmdline = NULL;
-	char **cmdargs;
-	char prompt[] = "(hsh) ";
-	Bool status = true;
+	unsigned int i;
 
-	name = (argv[0] != NULL) ? argv[0] : NULL;
-
-	while (status)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		write(1, prompt, strlen(prompt));
-		cmdline = get_user_input();
+		free(datash->_environ[i]);
 	}
 
-	return (0);
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
